5e9633e35e0db3941c9df6e2f0762c5d388b19d6
fomjar <fomjar@gmail.com>
2017-11-15 17:26:19 +0800

    framework

--- OneHeart-ios/OneHeart/controller/ReadController.swift
+++ OneHeart-ios/OneHeart/controller/ReadController.swift
@@ -18,8 +18,8 @@ class ReadController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(switchView)))
-        
+        self.view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(switchGesture)))
+
         self.vCurrent = self.vRead
     }
     
@@ -42,7 +42,7 @@ class ReadController: UIViewController {
     var switchValue = CGFloat(8)
     var switchTime  = Double(0.4)
     
-    @objc private func switchView(gesture: UIPanGestureRecognizer) {
+    @objc private func switchGesture(gesture: UIPanGestureRecognizer) {
         let p = gesture.translation(in: self.view)
         switch gesture.state {
         case .began:
@@ -55,39 +55,47 @@ class ReadController: UIViewController {
                 {left = 0}
             self.lcOffsetX.constant = left
         case .ended:
-            switch self.vCurrent! {
-            case self.vRead!:
-                if -self.vRead.frame.origin.x * self.switchValue > self.vRead.frame.width
-                    {self.switchToWrite()}
-                else
-                    {self.switchToRead()}
-            case self.vWrite!:
-                if -self.vRead.frame.origin.x / (self.switchValue - 1) * self.switchValue < self.vRead.frame.width
-                    {self.switchToRead()}
-                else
-                    {self.switchToWrite()}
-            default:
-                print()
-            }
+            self.switchView()
+        default:
+            print()
+        }
+    }
+    
+    @objc private func switchView() {
+        switch self.vCurrent! {
+        case self.vRead!:
+            if -self.vRead.frame.origin.x * self.switchValue > self.vRead.frame.width
+            {self.switchToWrite()}
+            else
+            {self.switchToRead()}
+        case self.vWrite!:
+            if -self.vRead.frame.origin.x / (self.switchValue - 1) * self.switchValue < self.vRead.frame.width
+            {self.switchToRead()}
+            else
+            {self.switchToWrite()}
         default:
             print()
         }
     }
     
     private func switchToRead() {
+        self.view.layer.removeAllAnimations()
+        
         self.vCurrent = self.vRead
         self.lcOffsetX.constant = 0
-        UIView.animate(withDuration: switchTime) {
+        UIView.animate(withDuration: switchTime, delay: 0, options: .allowUserInteraction, animations: {
             self.view.layoutIfNeeded()
-        }
+        })
     }
 
     private func switchToWrite() {
+        self.view.layer.removeAllAnimations()
+        
         self.vCurrent = self.vWrite
         self.lcOffsetX.constant = -self.vRead.frame.width
-        UIView.animate(withDuration: switchTime) {
+        UIView.animate(withDuration: switchTime, delay: 0, options: .allowUserInteraction, animations: {
             self.view.layoutIfNeeded()
-        }
+        })
     }
     
     @IBAction func sendIntention(_ sender: Any) {
